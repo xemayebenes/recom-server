ab3a1e01f6f6cd42cc49b58a7da8750701d17f2f
xema yebenes gomez <xema@devgurus.io>
2018-07-16 20:45:09 +0200

    process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { graphQLServer, engine } from './app';
 
-const GRAPHQL_PORT = 3001;
+const GRAPHQL_PORT = process.env.PORT || 3001;
 
-engine.listen({
+engine.listen(
+  {
     port: GRAPHQL_PORT,
     graphqlPaths: ['/graphql'],
     expressApp: graphQLServer,
     launcherOptions: {
-        startupTimeout: 3000,
-    },
-}, () => {
-    console.log(`Listening on port ${GRAPHQL_PORT}`)
-})
+      startupTimeout: 3000
+    }
+  },
+  () => {
+    console.log(`Listening on port ${GRAPHQL_PORT}`);
+  }
+);
