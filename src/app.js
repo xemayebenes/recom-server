f0f60dee1f7c94c359896dd771c1a2872ab8e877
xema yebenes gomez <xema@devgurus.io>
2018-07-16 21:37:50 +0200

    CORS MIDDLEWARE

--- src/app.js
+++ src/app.js
@@ -86,7 +86,7 @@ graphQLServer.post('/token', bodyParser.json(), async (req, res) => {
 graphQLServer.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
